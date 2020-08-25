40c7d1256e75076a219088fe8cb78cb7665dc5eb
VidensKnowledge <devydoolander@gmail.com>
2020-08-24 21:48:39 -0600

    some additions for submission

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-resources :notes
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'static_pages#home'
+
+  get '/about', to: 'static_pages#about'
+
+  resources :pages
 end
